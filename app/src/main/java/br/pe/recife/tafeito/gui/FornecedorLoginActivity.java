d32ff215a53c22043b861c132b1d2b1779019e88
fred-monier <frederico.monier@gmail.com>
2017-11-25 18:09:24 -0300

    Commit em 25/11/2017

---
+++ app/src/main/java/br/pe/recife/tafeito/gui/FornecedorLoginActivity.java
@@ -0,0 +1,149 @@
+package br.pe.recife.tafeito.gui;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import br.pe.recife.tafeito.R;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class FornecedorLoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+    @InjectView(R.id.input_email) EditText _emailText;
+    @InjectView(R.id.input_password) EditText _passwordText;
+    @InjectView(R.id.btn_login) Button _loginButton;
+    @InjectView(R.id.link_signup) TextView _signupLink;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_fornecedor_login);
+
+        ButterKnife.inject(this);
+
+        _loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        _signupLink.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                //Chama a tela de registro de fornecedor
+                Intent intent = new Intent(getApplicationContext(), FornecedorRegistroActivity.class);
+                startActivityForResult(intent, REQUEST_SIGNUP);
+            }
+        });
+    }
+
+    public void login() {
+        Log.d(TAG, "Login");
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        _loginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(FornecedorLoginActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage(getApplicationContext().getResources().
+                getText(R.string.login_autenticando).toString());
+        progressDialog.show();
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        // TODO: ********* AUTENTICAR O FORNECEDOR AQUI
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // Escolher se o login foi bem sucedido ou não para chamar o método adequado abaixo
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+
+                // TODO: ********* DECIDIR O QUE ACONTECE CASO O REGISTRO TENHA SIDO BEM SUCEDIDO
+                // By default we just finish the Activity and log them in automatically
+
+                this.finish();
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        //Impede o comando de voltar para activity anterior, principal
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+        _loginButton.setEnabled(true);
+        finish();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), getApplicationContext().getResources().
+                getText(R.string.login_falhou).toString(), Toast.LENGTH_LONG).show();
+
+        _loginButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError(getApplicationContext().getResources().
+                    getText(R.string.login_email_invalido).toString());
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError(getApplicationContext().getResources().
+                    getText(R.string.login_senha_tamanho_invalido).toString());
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        return valid;
+    }
+}
+
+
---
+++ app/src/main/java/br/pe/recife/tafeito/gui/FornecedorRegistroActivity.java
@@ -0,0 +1,135 @@
+package br.pe.recife.tafeito.gui;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import br.pe.recife.tafeito.R;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class FornecedorRegistroActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignupActivity";
+
+    @InjectView(R.id.input_name) EditText _nameText;
+    @InjectView(R.id.input_email) EditText _emailText;
+    @InjectView(R.id.input_password) EditText _passwordText;
+    @InjectView(R.id.btn_signup) Button _signupButton;
+    @InjectView(R.id.link_login) TextView _loginLink;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_fornecedor_registro);
+
+        ButterKnife.inject(this);
+
+        _signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+
+        _loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Voltar para a tela de Login
+                finish();
+            }
+        });
+    }
+
+    public void signup() {
+        Log.d(TAG, "Registro");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        _signupButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(FornecedorRegistroActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage(getApplicationContext().getResources().
+                getText(R.string.registro_criando_conta).toString());
+        progressDialog.show();
+
+        String name = _nameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        // TODO: ********* CADASTRAR O FORNECEDOR AQUI
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onSignupSuccess or onSignupFailed
+                        // depending on success
+                        onSignupSuccess();
+                        // onSignupFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    public void onSignupSuccess() {
+        _signupButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), getApplicationContext().getResources().
+                getText(R.string.registro_criando_conta).toString(), Toast.LENGTH_LONG).show();
+
+        _signupButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = _nameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if (name.isEmpty() || name.length() < 3 || name.length() > 50) {
+            _nameText.setError(getApplicationContext().getResources().
+                    getText(R.string.registro_nome_invalido).toString());
+            valid = false;
+        } else {
+            _nameText.setError(null);
+        }
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError(getApplicationContext().getResources().
+                    getText(R.string.login_email_invalido).toString());
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError(getApplicationContext().getResources().
+                    getText(R.string.login_senha_tamanho_invalido).toString());
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        return valid;
+    }
+}
